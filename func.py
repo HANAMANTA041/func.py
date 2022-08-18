0f510babd63e632f31e428168e14d006b70aa7fc
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-18 11:26:39 +0530

    func.md
    Functions in python are having both the when function defining (parameters )and the function call( arguments)

---
+++ func.py
@@ -0,0 +1,15 @@
+#Functions :block of code which only will run when we call it
+def trialfunction():
+    print("Hello,World!")
+
+trialfunction()    #function call
+def demofuction(x):#parameter
+    print(x+ " World")
+
+demofuction("yello")#argument
+#parameter both passes information,for a function its a veriable that is listed inside the parantheses in the function defined
+#argument after calling the function what we pass through the argument its a argument
+def testfunction(x,y):#multiparameter
+    print(x+ " "+y)
+
+testfunction("Blue","World")#multiargument if we give only one argument for two parameter typeError
